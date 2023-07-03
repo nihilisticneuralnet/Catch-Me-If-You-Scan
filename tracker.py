4ce513b40c08f47920d338e7f7d7508aac323392
nihilisticneuralnet <138315505+nihilisticneuralnet@users.noreply.github.com>
2023-07-03 19:19:37 +0530

    Add files via upload

---
+++ tracker.py
@@ -0,0 +1,40 @@
+import collections
+import math
+import time
+
+class tracker:
+    def __init__(self):
+        self.id_count = 0
+        self.center_points = {}
+
+    def update(self, objects_rect):
+        objects_bbs_ids = []
+        for rect in objects_rect:
+            x, y, w, h, index = rect
+            cx = (x + x + w) // 2
+            cy = (y + y + h) // 2
+
+            same_object_detected = False
+            for id, pt in self.center_points.items():
+                dist = math.hypot(cx - pt[0], cy - pt[1])
+
+                if dist < 25:
+                    self.center_points[id] = (cx, cy)
+                    # print(self.center_points)
+                    objects_bbs_ids.append([x, y, w, h, id, index])
+                    same_object_detected = True
+                    break
+
+            if same_object_detected is False:
+                self.center_points[self.id_count] = (cx, cy)
+                objects_bbs_ids.append([x, y, w, h, self.id_count, index])
+                self.id_count += 1
+
+        new_center_points = {}
+        for obj_bb_id in objects_bbs_ids:
+            _, _, _, _, object_id, index = obj_bb_id
+            center = self.center_points[object_id]
+            new_center_points[object_id] = center
+
+        self.center_points = new_center_points.copy()
+        return objects_bbs_ids
